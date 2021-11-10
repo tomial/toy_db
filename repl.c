cb3073dc9a0d8fb769c8af39178f4c6e74f620c5
tomial <b1250284002@hotmail.com>
2021-11-10 13:57:13 +0800

    finished chapter 9

--- repl.c
+++ repl.c
@@ -37,7 +37,11 @@ typedef struct {
   bool end_of_table; // Indicate a position one past the last element
 } Cursor;
 
-typedef enum { EXECUTE_SUCCESS, EXECUTE_TABLE_FULL } ExecuteResult;
+typedef enum {
+  EXECUTE_SUCCESS,
+  EXECUTE_DUPLICATE_KEY,
+  EXECUTE_TABLE_FULL
+} ExecuteResult;
 
 typedef enum { NODE_INTERNAL, NODE_LEAF } NodeType;
 
@@ -45,6 +49,9 @@ void* get_page(Pager* pager, uint32_t page_num);
 void db_close(Table* table);
 void print_constants(void);
 void print_leaf_node(void* node);
+Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key);
+NodeType get_node_type(void* node);
+void set_node_type(void* node, NodeType type);
 
 InputBuffer* new_input_buffer() {
   InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
@@ -132,6 +139,7 @@ const uint32_t PAGE_SIZE = 4096;
 
 /*
  * Common Node Header Layout
+ * 共用header属性
  */
 const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
 const uint32_t NODE_TYPE_OFFSET = 0;
@@ -143,6 +151,7 @@ const uint8_t COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_P
 
 /*
  * Leaf Node Header Layout
+ * 叶结点header属性
  */
 const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
 const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
@@ -150,15 +159,27 @@ const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_C
 
 /*
  * Leaf Node Body Layout
+ * 叶节点body属性
  */
 const uint32_t LEAF_NODE_KEY_SIZE = sizeof(uint32_t);
 const uint32_t LEAF_NODE_KEY_OFFSET = 0;
 const uint32_t LEAF_NODE_VALUE_SIZE = ROW_SIZE;
 const uint32_t LEAF_NODE_VALUE_OFFSET = LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE;
-const uint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
+const uint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE; // cell = key + value
 const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
-const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
+const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE; // 每页最多存放cells的数量
 
+NodeType get_node_type(void* node) {
+  uint8_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
+  return (NodeType)value;
+}
+
+void set_node_type(void* node, NodeType type) {
+  uint8_t value = type;
+  *((uint8_t*)(node + NODE_TYPE_OFFSET)) = value;
+}
+
+// cell里记录的数量
 uint32_t* leaf_node_num_cells(void* node) {
   return node + LEAF_NODE_NUM_CELLS_OFFSET;
 }
@@ -176,6 +197,7 @@ void* leaf_node_value(void* node, uint32_t cell_num) {
 }
 
 void initialize_leaf_node(void* node) {
+  set_node_type(node, NODE_LEAF);
   *leaf_node_num_cells(node) = 0;
 }
 
@@ -207,6 +229,39 @@ void print_leaf_node(void* node) {
     uint32_t key = *leaf_node_key(node, i);
     printf("  - %d : %d\n", i, key);
   }
+}
+
+Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key) {
+  void* node = get_page(table->pager, page_num);
+  uint32_t num_cells = (*leaf_node_num_cells(node));
+
+  Cursor* cursor = malloc(sizeof(Cursor));
+  cursor->table = table;
+  cursor->page_num = page_num;
+
+  // Binary search
+  uint32_t min_index = 0;
+  uint32_t one_past_max_index = num_cells;
+
+  while (one_past_max_index != min_index) {
+    uint32_t index = (one_past_max_index + min_index) / 2;
+    uint32_t key_at_index = *leaf_node_key(node, index);
+
+    if (key == key_at_index) {
+      cursor->cell_num = index;
+      return cursor;
+    }
+
+    if (key < key_at_index) {
+      one_past_max_index = index;
+    } else {
+      min_index = index + 1;
+    }
+  }
+
+  // one_past_max_index == min_index
+  cursor->cell_num = min_index;
+  return cursor;
 }
 
 void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value) {
@@ -297,17 +352,21 @@ Cursor* table_start(Table* table) {
   return cursor;
 }
 
-Cursor* table_end(Table* table) {
-  Cursor* cursor = malloc(sizeof(Cursor));
-  cursor->table = table;
+/*
+ * Return the position of the given key.
+ * If the key is not present
+ * return the position where it should be inserted
+ */
+Cursor* table_find(Table* table, uint32_t key) {
+  uint32_t root_page_num = table->root_page_num;
+  void* root_node = get_page(table->pager, root_page_num);
 
-  cursor->page_num = table->root_page_num;
-  void* root_node = get_page(table->pager, table->root_page_num);
-  uint32_t num_cells = *leaf_node_num_cells(root_node);
-  cursor->cell_num = num_cells;
-  cursor->end_of_table = true;
-
-  return cursor;
+  if (get_node_type(root_node) == NODE_LEAF) {
+    return leaf_node_find(table, root_page_num, key);
+  } else {
+    printf("Need to implement searching an internal node\n");
+    exit(EXIT_FAILURE);
+  }
 }
 
 void cursor_advance(Cursor* cursor) {
@@ -368,12 +427,21 @@ PrepareResult prepare_statement(InputBuffer* input_buffer,
 
 ExecuteResult execute_insert(Statement* statement, Table* table) {
   void* node = get_page(table->pager, table->root_page_num);
-  if ((*leaf_node_num_cells(node) >= LEAF_NODE_MAX_CELLS)) {
+  uint32_t num_cells = (*leaf_node_num_cells(node));
+  if (num_cells >= LEAF_NODE_MAX_CELLS) {
     return EXECUTE_TABLE_FULL;
   }
 
   Row* row_to_insert = &(statement->row_to_insert);
-  Cursor* cursor = table_end(table);
+  uint32_t key_to_insert = row_to_insert->id;
+  Cursor* cursor = table_find(table, key_to_insert);
+
+  if (cursor->cell_num < num_cells) {
+    uint32_t key_at_index = *leaf_node_key(node, cursor->cell_num);
+    if (key_at_index == key_to_insert) {
+      return EXECUTE_DUPLICATE_KEY;
+    }
+  }
 
   leaf_node_insert(cursor, row_to_insert->id, row_to_insert);
 
@@ -408,9 +476,9 @@ ExecuteResult execute_statement(Statement* statement, Table* table) {
 Pager* pager_open(const char* filename) {
   int fd = open(filename,
                 O_RDWR |      // Read/Write mode
-                    O_CREAT, // Create file if it does not exist
-               S_IWUSR |      // User write permission
-                    S_IRUSR   // User read permission
+                O_CREAT, // Create file if it does not exist
+                S_IWUSR |      // User write permission
+                S_IRUSR   // User read permission
                );
 
   if (fd == -1) {
@@ -558,6 +626,9 @@ int main(int argc, char* argv[]) {
     switch (execute_statement(&statement, table)) {
     case (EXECUTE_SUCCESS):
       printf("Executed.\n");
+      break;
+    case (EXECUTE_DUPLICATE_KEY):
+      printf("Error: Duplicate key.\n");
       break;
     case (EXECUTE_TABLE_FULL):
       printf("Error: Table full.\n");
